3adf9d246e00b5569248ba3a90924f6288954b92
SugiyamaSota <k024g0098@g.neec.ac.jp>
2025-05-16 16:15:03 +0900

    評価課題　完了

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@
 #include<dxgidebug.h>
 #include<dxcapi.h>
 #include"Convert.h"
+#include"Function.h"
 #include"externals/DirectXTex/DirectXTex.h"
 #include"externals/DirectXTex/d3dx12.h"
 #include<vector>
@@ -47,11 +48,6 @@ struct VertexData {
 	Vector2 texcoord;
 };
 
-void Log(std::ostream& os, const std::string& message) {
-	os << message << std::endl;
-	OutputDebugStringA(message.c_str());
-}
-
 static LONG WINAPI ExportDump(EXCEPTION_POINTERS* exception) {
 	//時刻を取得して、時刻を名前に入れたファイルを作成。Dumpsディレクトリ以下に出力
 	SYSTEMTIME time;
@@ -72,34 +68,6 @@ static LONG WINAPI ExportDump(EXCEPTION_POINTERS* exception) {
 	MiniDumpWriteDump(GetCurrentProcess(), processId, dumpFileHandle, MiniDumpNormal, &minidumpInformation, nullptr, nullptr);
 	//ほかに関連付けられているSEH例外ハンドラがあれば実行。通常はprocessを終了する
 	return EXCEPTION_EXECUTE_HANDLER;
-}
-
-std::wstring ConvertString(const std::string& str) {
-	if (str.empty()) {
-		return std::wstring();
-	}
-
-	auto sizeNeeded = MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&str[0]), static_cast<int>(str.size()), NULL, 0);
-	if (sizeNeeded == 0) {
-		return std::wstring();
-	}
-	std::wstring result(sizeNeeded, 0);
-	MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&str[0]), static_cast<int>(str.size()), &result[0], sizeNeeded);
-	return result;
-}
-
-std::string ConvertString(const std::wstring& str) {
-	if (str.empty()) {
-		return std::string();
-	}
-
-	auto sizeNeeded = WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), NULL, 0, NULL, NULL);
-	if (sizeNeeded == 0) {
-		return std::string();
-	}
-	std::string result(sizeNeeded, 0);
-	WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), result.data(), sizeNeeded, NULL, NULL);
-	return result;
 }
 
 IDxcBlob* CompileShader(
@@ -164,30 +132,6 @@ IDxcBlob* CompileShader(
 	shaderResult->Release();
 	//実行用のバイナリを返却
 	return shaderBlob;
-}
-
-ID3D12Resource* CreateBufferResource(ID3D12Device* device, size_t sizeInBytes) {
-	//ヒープの生成
-	D3D12_HEAP_PROPERTIES uploadHeapProperties{};
-	uploadHeapProperties.Type = D3D12_HEAP_TYPE_UPLOAD;
-	//リソースの設定
-	D3D12_RESOURCE_DESC ReourceDesc{};
-	//バッファリソース
-	ReourceDesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
-	ReourceDesc.Width = sizeInBytes;
-	//バッファの場合はこれらは1にする決まり
-	ReourceDesc.Height = 1;
-	ReourceDesc.DepthOrArraySize = 1;
-	ReourceDesc.MipLevels = 1;
-	ReourceDesc.SampleDesc.Count = 1;
-	//バッファの場合はこれにする決まり
-	ReourceDesc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
-	//実際に頂点リソースを作る
-	ID3D12Resource* Resource = nullptr;
-	HRESULT hr = device->CreateCommittedResource(&uploadHeapProperties, D3D12_HEAP_FLAG_NONE, &ReourceDesc, D3D12_RESOURCE_STATE_GENERIC_READ, nullptr, IID_PPV_ARGS(&Resource));
-	assert(SUCCEEDED(hr));
-
-	return Resource;
 }
 
 ID3D12DescriptorHeap* CreateDescriptorHeap(
