832109e1af0b350ca3abefac39029f1a62088605
SugiyamaSota <k024g0098@g.neec.ac.jp>
2025-05-23 14:56:33 +0900

    ファイル整理

---
+++ MyDirectX.cpp
@@ -0,0 +1,55 @@
+#include "MyDirectX.h"
+
+LRESULT CALLBACK MyDirectX::WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
+	if (ImGui_ImplWin32_WndProcHandler(hwnd, msg, wparam, lparam)) {
+		return true;
+	}
+	//メッセージに応じてゲーム固有の処理を行う
+	switch (msg) {
+		//ウィンドウが破棄された
+	case WM_DESTROY:
+		//OSに対して、アプリの終了を伝える
+		PostQuitMessage(0);
+		return 0;
+	}
+
+	//標準のメッセージ処理を行う
+	return DefWindowProc(hwnd, msg, wparam, lparam);
+}
+
+void MyDirectX::Initialize() {
+	WNDCLASS wc{};
+	//ウィンドウプロシージャ
+	wc.lpfnWndProc = WindowProc;
+	//ウィンドウクラス名
+	wc.lpszClassName = L"CG2WindowClass";
+	//インスタンスハンドル
+	wc.hInstance = GetModuleHandle(nullptr);
+	//カーソル
+	wc.hCursor = (LoadCursor(nullptr, IDC_ARROW));
+
+	//ウィンドウクラスを登録する
+	RegisterClass(&wc);
+
+	//ウィンドウの生成
+	HWND hwnd = CreateWindow(
+		wc.lpszClassName,
+		L"CG2",
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		wrc_.right - wrc_.left,
+		wrc_.bottom - wrc_.top,
+		nullptr,
+		nullptr,
+		wc.hInstance,
+		nullptr);
+}
+
+void MyDirectX::Update() {
+
+}
+
+void MyDirectX::Finalize() {
+
+}
