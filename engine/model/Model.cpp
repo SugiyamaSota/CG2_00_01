282aa7f9087d0d3507bd5dc1f0e7feac6e4cda68
SugiyamaSota <k024g0098@g.neec.ac.jp>
2025-06-19 06:18:39 +0900

    モデルのクラス化完了

--- engine/model/Model.cpp
+++ engine/model/Model.cpp
@@ -1,1 +1,156 @@
 #include "Model.h"
+#include"../common/DirectXCommon.h"
+#include <filesystem>
+#include <fstream>
+#include<cassert>
+#include"../function/Utility.h"
+
+#include"../../math/Convert.h"
+#include"../../math/Matrix.h"
+
+#include"../../debugCamera/DebugCamera.h"
+
+Model::Model(DirectXCommon* common) {
+	common_ = common;
+	projectionMatrix_ = MakePerspectiveFovMatrix(0.45f, float(1280) / float(720), 0.1f, 100.0f);
+}
+
+void Model::LoadModel(const std::string& directoryPath, const std::string& filename) {
+	// モデル読み込み
+	modelData_ = LoadObjFile(directoryPath, filename);
+	// 頂点用のリソース
+	vertexResource_ = CreateBufferResource(common_->GetDevice(), sizeof(VertexData) * modelData_.vertices.size());
+	vertexBufferView_.BufferLocation = vertexResource_->GetGPUVirtualAddress();
+	vertexBufferView_.SizeInBytes = UINT(sizeof(VertexData) * modelData_.vertices.size());
+	vertexBufferView_.StrideInBytes = sizeof(VertexData);
+	vertexResource_->Map(0, nullptr, reinterpret_cast<void**>(&vertexData_));
+	std::memcpy(vertexData_, modelData_.vertices.data(), sizeof(VertexData) * modelData_.vertices.size());
+
+	// マテリアル用のリソース
+	materialResource_ = CreateBufferResource(common_->GetDevice(), sizeof(Material));
+	materialData_ = nullptr;
+	materialResource_->Map(0, nullptr, reinterpret_cast<void**>(&materialData_));
+	materialData_->color = Vector4(1.0f, 1.0f, 1.0f, 1.0f);
+	materialData_->enableLighting = true;
+	materialData_->uvTransform = MakeIdentity4x4();
+
+	// WVP用のリソース
+	wvpResource_ = CreateBufferResource(common_->GetDevice(), sizeof(TransformationMatrix));
+	wvpData_ = nullptr;
+	wvpResource_->Map(0, nullptr, reinterpret_cast<void**>(&wvpData_));
+	wvpData_->WVP = MakeIdentity4x4();
+	wvpData_->World = MakeIdentity4x4();
+
+}
+
+void Model::Initialize(WorldTransform worldTransform) {
+	transform_ = worldTransform;
+}
+
+void Model::Update(WorldTransform worldTransform, DebugCamera* debugCamera) {
+	transform_ = worldTransform;
+	wvpData_->World = MakeAffineMatrix(transform_.scale, transform_.rotate, transform_.translate);
+	Matrix4x4 worldViewProjectionMatrix = Multiply(wvpData_->World, Multiply(debugCamera->GetViewMatrix(), projectionMatrix_));
+	wvpData_->WVP = worldViewProjectionMatrix;
+}
+
+void Model::Draw(D3D12_GPU_DESCRIPTOR_HANDLE handle) {
+	//モデルの描画
+	common_->GetCommandList()->IASetVertexBuffers(0, 1, &vertexBufferView_);//VBVを設定
+	//形状を設定。PSOに設定しているものとは別。同じものを設定るすると考える
+	common_->GetCommandList()->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+	//マテリアルCBufferの場所を設定
+	common_->GetCommandList()->SetGraphicsRootConstantBufferView(0, materialResource_->GetGPUVirtualAddress());
+	//wvp用のCBufferの場所を設定
+	common_->GetCommandList()->SetGraphicsRootConstantBufferView(1, wvpResource_->GetGPUVirtualAddress());
+	//SRVようのdescriptionTavleの先頭を設定
+	common_->GetCommandList()->SetGraphicsRootDescriptorTable(2, handle);
+	//描画!3頂点で1つのインスタンス
+	common_->GetCommandList()->DrawInstanced(UINT(modelData_.vertices.size()), 1, 0, 0);
+}
+
+//mtlファイルの読み込み
+MaterialData Model::LoadMaterialTemplateFile(const std::string& directoryPath, const std::string& filename) {
+	MaterialData materialData;
+	std::string line;
+	std::ifstream file(directoryPath + '/' + filename);
+	assert(file.is_open());
+
+	while (std::getline(file, line)) {
+		std::string identifier;
+		std::istringstream s(line);
+		s >> identifier;
+
+		//identifierに応じた処理
+		if (identifier == "map_Kd") {
+			std::string textureFilename;
+			s >> textureFilename;
+			//連結してファイルパスにする
+			materialData.textureFilepath = directoryPath + '/' + textureFilename;
+		}
+	}
+	return materialData;
+}
+
+//objファイルの読み込み
+ModelData Model::LoadObjFile(const std::string& directoryPath, const std::string& filename) {
+	ModelData modelData;
+	std::vector<Vector4>positions;
+	std::vector<Vector3>normals;
+	std::vector<Vector2>texcoords;
+	std::string line;
+
+	std::ifstream file(directoryPath + '/' + filename);
+	assert(file.is_open());
+
+	while (std::getline(file, line)) {
+		std::string identifier;
+		std::istringstream s(line);
+		s >> identifier;
+
+		if (identifier == "v") {
+			Vector4 position;
+			s >> position.x >> position.y >> position.z;
+			position.x *= -1.0f;
+			position.w = 1.0f;
+			positions.push_back(position);
+		} else if (identifier == "vt") {
+			Vector2 texcoord;
+			s >> texcoord.x >> texcoord.y;
+			texcoord.y = 1.0f - texcoord.y;
+			texcoords.push_back(texcoord);
+		} else if (identifier == "vn") {
+			Vector3 normal;
+			s >> normal.x >> normal.y >> normal.z;
+			normal.x *= -1.0f;
+			normals.push_back(normal);
+		} else if (identifier == "f") {
+			VertexData triangle[3];
+			for (int32_t faceVertex = 0; faceVertex < 3; ++faceVertex) {
+				std::string vertexDefinition;
+				s >> vertexDefinition;
+				std::istringstream v(vertexDefinition);
+				uint32_t elementIndices[3];
+				for (int32_t element = 0; element < 3; ++element) {
+					std::string index;
+					std::getline(v, index, '/');
+					elementIndices[element] = std::stoi(index);
+				}
+				Vector4 position = positions[elementIndices[0] - 1];
+				Vector2 texcoord = texcoords[elementIndices[1] - 1];
+				Vector3 normal = normals[elementIndices[2] - 1];
+				triangle[faceVertex] = { position,texcoord,normal };
+			}
+			modelData.vertices.push_back(triangle[2]);
+			modelData.vertices.push_back(triangle[1]);
+			modelData.vertices.push_back(triangle[0]);
+		} else if (identifier == "mtllib") {
+			std::string materialFilename;
+			s >> materialFilename;
+
+			modelData.material = LoadMaterialTemplateFile(directoryPath, materialFilename);
+		}
+	}
+	return modelData;
+}
+
