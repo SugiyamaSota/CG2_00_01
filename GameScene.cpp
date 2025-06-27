bd9cc0d27012b599db382b7d06c7f5e50f6039e3
SugiyamaSota <k024g0098@g.neec.ac.jp>
2025-06-27 15:20:43 +0900

    AL3_01_01

---
+++ GameScene.cpp
@@ -0,0 +1,13 @@
+#include "GameScene.h"
+
+void GameScene::Initialize() {
+	//ゲーム内の初期化処理を書く
+}
+
+void GameScene::Update() {
+	//ゲーム内の更新処理を書く
+}
+
+void GameScene::Draw() {
+	//ゲーム内の描画処理を書く
+}
---
+++ GameScene.h
@@ -0,0 +1,14 @@
+#pragma once
+#include"engine/bonjin/BonjinEngine.h"
+
+class GameScene {
+public:
+	//初期化
+	void Initialize();
+
+	//更新
+	void Update();
+
+	//描画
+	void Draw();
+};
