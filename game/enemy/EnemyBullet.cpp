6dd4602b0c6c3ce94ada4fa5c74a15dd894a6d67
SugiyamaSota <k024g0098@g.neec.ac.jp>
2025-07-22 20:45:02 +0900

    コードの修正

--- game/enemy/EnemyBullet.cpp
+++ game/enemy/EnemyBullet.cpp
@@ -14,22 +14,12 @@ void EnemyBullet::Initialize(Model* model, const Vector3& position, const Vector
 
 	velocity_ = velocity;
 
-	float pitch = 0.0f; // X軸周りの回転
-	float yaw = 0.0f;   // Y軸周りの回転
-	float roll = 0.0f;  // Z軸周りの回転 
-		yaw = std::atan2f(velocity_.x, velocity_.z);
+	worldTransform_.rotate.y= std::atan2f(velocity_.x, velocity_.z);
 
-		float horizontalLength = std::sqrtf(velocity_.x * velocity_.x + velocity_.z * velocity_.z);
+	float velocityXZ = std::sqrtf(velocity_.x * velocity_.x + velocity_.z * velocity_.z);
 
-		if (horizontalLength < 0.0001f) {
-			pitch = std::atan2f(velocity_.y, 0.0f);
-		} else {
-			pitch = std::atan2f(velocity_.y, horizontalLength);
-		}
+	worldTransform_.rotate.x = std::atan2(velocity_.y, velocityXZ);
 
-		roll = 0.0f;
-	
-	worldTransform_.rotate = { pitch, -yaw, roll };
 }
 
 EnemyBullet::~EnemyBullet() {
