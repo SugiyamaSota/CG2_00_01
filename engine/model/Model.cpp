703248afe4acd4043bc07359636d3ba280b0fc8c
SugiyamaSota <k024g0098@g.neec.ac.jp>
2025-10-10 11:10:21 +0900

    リファクタリング->モデル

--- engine/model/Model.cpp
+++ engine/model/Model.cpp
@@ -1,17 +1,7 @@
 #include "Model.h"
-#include"../common/DirectXCommon.h"
-#include <filesystem>
-#include <fstream>
-#include<cassert>
-#include"../function/Utility.h"
-
-#include"../math/Convert.h"
-#include"../math/Matrix.h"
-
-#include"../camera/Camera.h"
-#include"../texture/TextureManager.h"
 
 Model::Model() {
+	common = DirectXCommon::GetInstance();
 	transform_ = InitializeWorldTransform();
 	viewMatrix_ = MakeIdentity4x4();
 	projectionMatrix_ = MakePerspectiveFovMatrix(0.45f, float(1280) / float(720), 0.1f, 100.0f);
@@ -19,10 +9,11 @@ Model::Model() {
 }
 
 void Model::LoadModel(const std::string& fileName) {
-	// モデル読み込み
+	// モデルファイル読み込み
 	modelData_ = LoadObjFile("resources/models/" + fileName, fileName + ".obj");
+
 	// 頂点用のリソース
-	vertexResource_ = CreateBufferResource(DirectXCommon::GetInstance()->GetDevice(), sizeof(VertexData) * modelData_.vertices.size());
+	vertexResource_ = CreateBufferResource(common->GetDevice(), sizeof(VertexData) * modelData_.vertices.size());
 	vertexBufferView_.BufferLocation = vertexResource_->GetGPUVirtualAddress();
 	vertexBufferView_.SizeInBytes = UINT(sizeof(VertexData) * modelData_.vertices.size());
 	vertexBufferView_.StrideInBytes = sizeof(VertexData);
@@ -30,7 +21,7 @@ void Model::LoadModel(const std::string& fileName) {
 	std::memcpy(vertexData_, modelData_.vertices.data(), sizeof(VertexData) * modelData_.vertices.size());
 
 	// マテリアル用のリソース
-	materialResource_ = CreateBufferResource(DirectXCommon::GetInstance()->GetDevice(), sizeof(Material));
+	materialResource_ = CreateBufferResource(common->GetDevice(), sizeof(Material));
 	materialData_ = nullptr;
 	materialResource_->Map(0, nullptr, reinterpret_cast<void**>(&materialData_));
 	materialData_->color = Vector4(1.0f, 1.0f, 1.0f, 1.0f);
@@ -38,7 +29,7 @@ void Model::LoadModel(const std::string& fileName) {
 	materialData_->uvTransform = MakeIdentity4x4();
 
 	// WVP用のリソース
-	wvpResource_ = CreateBufferResource(DirectXCommon::GetInstance()->GetDevice(), sizeof(TransformationMatrix));
+	wvpResource_ = CreateBufferResource(common->GetDevice(), sizeof(TransformationMatrix));
 	wvpData_ = nullptr;
 	wvpResource_->Map(0, nullptr, reinterpret_cast<void**>(&wvpData_));
 	wvpData_->WVP = MakeIdentity4x4();
@@ -46,7 +37,7 @@ void Model::LoadModel(const std::string& fileName) {
 
 	// テクスチャ
 	textureHandle_ = TextureManager::GetInstance()->LoadTexture(modelData_.material.textureFilepath);
-	DirectXCommon::GetInstance()->WaitAndResetCommandList();
+	common->WaitAndResetCommandList();
 	TextureManager::GetInstance()->ReleaseIntermediateResources();
 }
 
@@ -60,49 +51,26 @@ void Model::Update(WorldTransform worldTransform, Camera* camera) {
 
 void Model::Draw() {
 	// PSOの設定
-	DirectXCommon::GetInstance()->GetCommandList()->SetGraphicsRootSignature(DirectXCommon::GetInstance()->GetPSO()->GetDefaultRootSignature());
-	DirectXCommon::GetInstance()->GetCommandList()->SetPipelineState(DirectXCommon::GetInstance()->GetPSO()->GetDefaultPipelineState(BlendMode::kNone));
+	common->GetCommandList()->SetGraphicsRootSignature(common->GetPSO()->GetDefaultRootSignature());
+	common->GetCommandList()->SetPipelineState(common->GetPSO()->GetDefaultPipelineState(BlendMode::kNone));
 
-	//モデルの描画
-	DirectXCommon::GetInstance()->GetCommandList()->IASetVertexBuffers(0, 1, &vertexBufferView_);//VBVを設定
-	//形状を設定。PSOに設定しているものとは別。同じものを設定るすると考える
-	DirectXCommon::GetInstance()->GetCommandList()->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
-	//マテリアルCBufferの場所を設定
-	DirectXCommon::GetInstance()->GetCommandList()->SetGraphicsRootConstantBufferView(0, materialResource_->GetGPUVirtualAddress());
-	//wvp用のCBufferの場所を設定
-	DirectXCommon::GetInstance()->GetCommandList()->SetGraphicsRootConstantBufferView(1, wvpResource_->GetGPUVirtualAddress());
-	//SRVようのdescriptionTavleの先頭を設定
-	DirectXCommon::GetInstance()->GetCommandList()->SetGraphicsRootDescriptorTable(2, TextureManager::GetInstance()->GetGPUHandle(textureHandle_));
-	//光
-	DirectXCommon::GetInstance()->GetCommandList()->SetGraphicsRootConstantBufferView(3, DirectXCommon::GetInstance()->GetDirectionalLightResource()->GetGPUVirtualAddress());
-	//描画!
-	DirectXCommon::GetInstance()->GetCommandList()->DrawInstanced(UINT(modelData_.vertices.size()), 1, 0, 0);
+	//　モデルの描画
+	// VBV
+	common->GetCommandList()->IASetVertexBuffers(0, 1, &vertexBufferView_);
+	// 形状を設定
+	common->GetCommandList()->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+	// マテリアルCBufferの場所を設定
+	common->GetCommandList()->SetGraphicsRootConstantBufferView(0, materialResource_->GetGPUVirtualAddress());
+	// wvp用のCBufferの場所を設定
+	common->GetCommandList()->SetGraphicsRootConstantBufferView(1, wvpResource_->GetGPUVirtualAddress());
+	// SRV用のdescriptionTavleの先頭を設定
+	common->GetCommandList()->SetGraphicsRootDescriptorTable(2, TextureManager::GetInstance()->GetGPUHandle(textureHandle_));
+	// 光
+	common->GetCommandList()->SetGraphicsRootConstantBufferView(3, common->GetDirectionalLightResource()->GetGPUVirtualAddress());
+	// 描画
+	common->GetCommandList()->DrawInstanced(UINT(modelData_.vertices.size()), 1, 0, 0);
 }
 
-//mtlファイルの読み込み
-MaterialData Model::LoadMaterialTemplateFile(const std::string& directoryPath, const std::string& filename) {
-	MaterialData materialData;
-	std::string line;
-	std::ifstream file(directoryPath + '/' + filename);
-	assert(file.is_open());
-
-	while (std::getline(file, line)) {
-		std::string identifier;
-		std::istringstream s(line);
-		s >> identifier;
-
-		//identifierに応じた処理
-		if (identifier == "map_Kd") {
-			std::string textureFilename;
-			s >> textureFilename;
-			//連結してファイルパスにする
-			materialData.textureFilepath = directoryPath + '/' + textureFilename;
-		}
-	}
-	return materialData;
-}
-
-//objファイルの読み込み
 ModelData Model::LoadObjFile(const std::string& directoryPath, const std::string& filename) {
 	ModelData modelData;
 	std::vector<Vector4>positions;
@@ -162,4 +130,26 @@ ModelData Model::LoadObjFile(const std::string& directoryPath, const std::string
 		}
 	}
 	return modelData;
+}
+
+MaterialData Model::LoadMaterialTemplateFile(const std::string& directoryPath, const std::string& filename) {
+	MaterialData materialData;
+	std::string line;
+	std::ifstream file(directoryPath + '/' + filename);
+	assert(file.is_open());
+
+	while (std::getline(file, line)) {
+		std::string identifier;
+		std::istringstream s(line);
+		s >> identifier;
+
+		//identifierに応じた処理
+		if (identifier == "map_Kd") {
+			std::string textureFilename;
+			s >> textureFilename;
+			//連結してファイルパスにする
+			materialData.textureFilepath = directoryPath + '/' + textureFilename;
+		}
+	}
+	return materialData;
 }
